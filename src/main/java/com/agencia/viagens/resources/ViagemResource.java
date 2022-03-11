4dcf3133aa6f818bd75c058c3c48e2622f3b7dd8
barbarabrito <barbaramabrito@gmail.com>
2022-03-10 21:14:19 -0300

    add cross orign annotation

--- src/main/java/com/agencia/viagens/resources/ViagemResource.java
+++ src/main/java/com/agencia/viagens/resources/ViagemResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.agencia.viagens.model.Viagem;
 import com.agencia.viagens.repository.ViagemRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value="/api")
 public class ViagemResource {
